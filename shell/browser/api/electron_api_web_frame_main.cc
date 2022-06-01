8855fd6bcac350e8d99b63662a8693773f847659
Shelley Vohr <shelley.vohr@gmail.com>
2022-06-01 11:34:07 +0200

    3665762: Remove RenderFrameHost::IsRenderFrameCreated from //content/.
    https://chromium-review.googlesource.com/c/chromium/src/+/3665762

--- shell/browser/api/electron_api_web_frame_main.cc
+++ shell/browser/api/electron_api_web_frame_main.cc
@@ -194,7 +194,7 @@ void WebFrameMain::MaybeSetupMojoConnection() {
         &WebFrameMain::OnRendererConnectionError, weak_factory_.GetWeakPtr()));
   }
   // Wait for RenderFrame to be created in renderer before accessing remote.
-  if (pending_receiver_ && render_frame_->IsRenderFrameCreated()) {
+  if (pending_receiver_ && render_frame_->IsRenderFrameLive()) {
     render_frame_->GetRemoteInterfaces()->GetInterface(
         std::move(pending_receiver_));
   }
